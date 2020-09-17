c7ef200c833ea0e907df760e61b806535f46b635
Roni Frantchi <roni.frantchi@gmail.com>
2020-09-17 15:46:27 +0300

    rename network

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -19,7 +19,7 @@ data "vsphere_resource_pool" "pool" {
 }
 
 data "vsphere_network" "network" {
-  name          = "public"
+  name          = "New port group"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
