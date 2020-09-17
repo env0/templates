2fb0026c457140a38901a70f141ee25d364309d2
Roni Frantchi <roni.frantchi@gmail.com>
2020-09-17 15:33:10 +0300

    add template data

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -5,7 +5,7 @@ provider "vsphere" {
 }
 
 data "vsphere_datacenter" "dc" {
-  name = "dc1"
+  name = "Datacenter"
 }
 
 data "vsphere_datastore" "datastore" {
@@ -14,7 +14,7 @@ data "vsphere_datastore" "datastore" {
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/Resources"
+  name          = "Cluster1/Resources"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
@@ -25,19 +25,43 @@ data "vsphere_network" "network" {
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = 2
   memory   = 1024
-  guest_id = "other3xLinux64Guest"
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
 
   network_interface {
-   network_id = data.vsphere_network.network.id
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
 
   disk {
-    label = "disk0"
-    size  = 20
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    # customize {
+    #   linux_options {
+    #     host_name = "terraform-test"
+    #     domain    = "test.internal"
+    #   }
+
+    #   network_interface {
+    #     ipv4_address = "10.0.0.10"
+    #     ipv4_netmask = 24
+    #   }
+
+    #   ipv4_gateway = "10.0.0.1"
+    # }
+  }
+}
 }
