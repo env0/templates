8920c47c428bf9c95de18fae4205c6ee3b8091b7
Roni Frantchi <roni.frantchi@gmail.com>
2020-09-17 13:45:02 +0300

    comment out data and creds

---
+++ vsphere/main.tf
@@ -0,0 +1,46 @@
+provider "vsphere" {
+#   user           = var.vsphere_user
+#   password       = var.vsphere_password
+#   vsphere_server = var.vsphere_server
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+# data "vsphere_datacenter" "dc" {
+#   name = "dc1"
+# }
+
+# data "vsphere_datastore" "datastore" {
+#   name          = "datastore1"
+#   datacenter_id = data.vsphere_datacenter.dc.id
+# }
+
+# data "vsphere_resource_pool" "pool" {
+#   name          = "cluster1/Resources"
+#   datacenter_id = data.vsphere_datacenter.dc.id
+# }
+
+# data "vsphere_network" "network" {
+#   name          = "public"
+#   datacenter_id = data.vsphere_datacenter.dc.id
+# }
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  # resource_pool_id = data.vsphere_resource_pool.pool.id
+  # datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  # network_interface {
+  #  network_id = data.vsphere_network.network.id
+  # }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
