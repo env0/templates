bcb017ad1b95f0c1b0e6284ec52ac62e10c5d5cf
Roni Frantchi <roni.frantchi@gmail.com>
2020-09-21 15:17:35 +0300

    remove the do not wait flag

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -32,7 +32,6 @@ resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
-  wait_for_guest_net_timeout = 0
   num_cpus = 2
   memory   = 1024
   guest_id = data.vsphere_virtual_machine.template.guest_id
