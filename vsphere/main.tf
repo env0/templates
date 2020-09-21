b28c4c9734eda1df386c59b77ac10b249d8074e0
Roni Frantchi <roni.frantchi@gmail.com>
2020-09-21 09:53:24 +0300

    remove bad char

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -18,7 +18,7 @@ data "vsphere_resource_pool" "pool" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_network" "network" {d
+data "vsphere_network" "network" {
   name          = "New port group"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
