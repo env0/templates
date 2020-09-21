4c6f24fa238125d18204a2bcb6330be6fb9cef0f
Roni Frantchi <roni.frantchi@gmail.com>
2020-09-21 12:52:09 +0300

    revert

--- vsphere/pass.sh
+++ vsphere/pass.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/expect -f
+#!/usr/bin/expect
 
 set timeout 20
 
+set cmd [lrange $argv 1 end]
+set password [lindex $argv 0]
+
+eval spawn $cmd
 expect "assword:"
 send "$SSH_PASSWORD\r";
 interact
