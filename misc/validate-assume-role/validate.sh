14256f7c03280ab81dbab64f821b598067bb81fa
avner.sorek <avner.sorek@env0.com>
2021-03-14 17:20:31 +0200

    use script

---
+++ misc/validate-assume-role/validate.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+[[ "$EXPECTED_ARN" == "" ]] && echo "Missing var EXPECTED_ARN" && exit 1
+
+export ASSUMED_ARN=$(aws sts get-caller-identity --query Arn)
+
+[[ ! "$ASSUMED_ARN" =~ "$EXPECTED_ARN" ]] && echo "Assumed role expectation failed" && exit 1
