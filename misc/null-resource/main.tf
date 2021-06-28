9db2b8ab2c7f8789990b3f23b715c8e1395f65c1
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2021-06-28 18:16:17 +0300

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,22 +1,3 @@
 resource "null_resource" "null" {
 }
-resource "null_resource" "null2" {
-}
-resource "null_resource" "null3" {
-}
-resource "null_resource" "null4" {
-}
-resource "null_resource" "null5" {
-}
-resource "null_resource" "null6" {
-}
-resource "null_resource" "null7" {
-}
-resource "null_resource" "null8" {
-}
-resource "null_resource" "null9" {
-}
-resource "null_resource" "null10" {
-}
-resource "null_resource" "null11" {
-}
+
