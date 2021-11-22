f4dfb53a968dc8fea1fa1bfe2d6a950e03893308
Hever Farber <hever.farber@gmail.com>
2021-11-22 17:04:08 +0200

    wip

--- aws/github-ee/main.tf
+++ aws/github-ee/main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "github_ee" {
   vpc_id                  = var.vpc_id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "172.31.150.0/8"
   map_public_ip_on_launch = true
 
   tags = {
