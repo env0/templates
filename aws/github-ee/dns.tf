58e75ae5ad3232a9338513f4a1aa01cc7260164a
Hever Farber <hever.farber@gmail.com>
2021-11-22 17:34:40 +0200

    wip

--- aws/github-ee/dns.tf
+++ aws/github-ee/dns.tf
@@ -1,13 +1,7 @@
-/*
-resource "aws_route53_record" "redash-app-dashboard-env0-dot-com-A-record" {
+resource "aws_route53_record" "github-ee-record" {
   name    = "github-ee.dev.env0.com"
   type    = "A"
   zone_id = var.aws_route53_zone_id
-
-  alias {
-    evaluate_target_health = true
-    name                   = aws_elb.redash.dns_name
-    zone_id                = aws_elb.redash.zone_id
-  }
+  ttl     = "300"
+  records = [aws_instance.github_ee.public_ip]
 }
-*/
