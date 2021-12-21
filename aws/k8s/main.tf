a3be015e0c185cf2eb6369057a22749577c285f3
Hever Farber <hever.farber@gmail.com>
2021-12-21 11:13:38 +0200

    k8s

--- aws/k8s/main.tf
+++ aws/k8s/main.tf
@@ -2,6 +2,27 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
 module "my-cluster" {
   source = "git::github.com:env0/k8s-modules.git//aws?ref=feat-add-aws-modules"
 
