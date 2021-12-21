f2e179ef4f126164e00a34322683a01fdcaf8908
Hever Farber <hever.farber@gmail.com>
2021-12-21 18:33:46 +0200

    helm

--- aws/k8s/main.tf
+++ aws/k8s/main.tf
@@ -23,6 +23,18 @@ provider "kubectl" {
   load_config_file       = false
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
 module "my-cluster" {
   source = "github.com/env0/k8s-modules//aws?ref=feat-add-aws-modules"
 
