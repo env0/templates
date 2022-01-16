85c59bc452da4dc5a0fb964b93344e4f30e14556
tomporat247 <tomporat247@gmail.com>
2022-01-16 16:48:16 +0200

    add external icon

--- misc/slack-notification/slack.sh
+++ misc/slack-notification/slack.sh
@@ -14,6 +14,7 @@ else
   MESSAGE="${MESSAGE} Approved by: ${ENV0_REVIEWER_NAME}(${ENV0_REVIEWER_EMAIL})"
 fi
 
-PAYLOAD="{\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"webhookbot\", \"text\": \"${MESSAGE}\", \"icon_emoji\": \":env0:\"}"
+ICON_URL="https://uploads-ssl.webflow.com/5ceab5395d0f478e169de7c0/5f21eb54b57c7a8559c68ff7_Env0-Color%20Icon%403x.png"
+PAYLOAD="{\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"webhookbot\", \"text\": \"${MESSAGE}\", \"icon_url\": \"${ICON_URL}\"}"
 
 curl -X POST --data-urlencode "payload=${PAYLOAD}" ${SLACK_URL}
