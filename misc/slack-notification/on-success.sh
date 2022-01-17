6c125ad231f09af55852c7256792340221350ab2
tomporat247 <tomporat247@gmail.com>
2022-01-17 14:15:09 +0200

    add approve notification

--- misc/slack-notification/on-success.sh
+++ misc/slack-notification/on-success.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-MESSAGE="Successfully finished running '${ENV0_DEPLOYMENT_TYPE}' deployment.\n"
-
-if [[ -z "${ENV0_REVIEWER_NAME}" ]]; then
-  MESSAGE="${MESSAGE} Automatically approved."
-else
-  MESSAGE="${MESSAGE} Approved by: ${ENV0_REVIEWER_NAME}(${ENV0_REVIEWER_EMAIL})"
-fi
+MESSAGE="Successfully finished running '${ENV0_DEPLOYMENT_TYPE}' deployment."
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${__dir}"/slack.sh "$MESSAGE"
