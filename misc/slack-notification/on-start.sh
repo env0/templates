49b406ab9a229d72da602f35c91962b2143b5f75
tomporat247 <tomporat247@gmail.com>
2022-01-17 12:13:04 +0200

    remove unnecessary space

--- misc/slack-notification/on-start.sh
+++ misc/slack-notification/on-start.sh
@@ -3,9 +3,9 @@
 MESSAGE="Started running '${ENV0_DEPLOYMENT_TYPE}' on"
 
 if [[ -z "${ENV0_DEPLOYMENT_REVISION}" ]]; then
-  MESSAGE="${MESSAGE} repository's default branch.\n"
+  MESSAGE="${MESSAGE} repository's default branch"
 else
-  MESSAGE="${MESSAGE} revision '${ENV0_DEPLOYMENT_REVISION}'.\n"
+  MESSAGE="${MESSAGE} revision '${ENV0_DEPLOYMENT_REVISION}'"
 fi
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
