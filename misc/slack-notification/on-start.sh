b40c4306f4766f87a2bac3b2782bea984d769208
tomporat247 <tomporat247@gmail.com>
2022-01-17 14:29:09 +0200

    use better phrasing

--- misc/slack-notification/on-start.sh
+++ misc/slack-notification/on-start.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-MESSAGE="Started running \`${ENV0_DEPLOYMENT_TYPE}\` deployment."
+if [[ -z "${ENV0_REVIEWER_NAME}" ]]; then
+  ACTION="Started"
+else
+  ACTION="Continued"
+fi
+
+MESSAGE="${ACTION} running \`${ENV0_DEPLOYMENT_TYPE}\` deployment."
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${__dir}"/slack.sh "$MESSAGE"
