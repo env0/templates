c804a52bf3d0e1a6676f57550bfb2b1fa5fdf6b2
tomporat247 <tomporat247@gmail.com>
2022-01-17 14:41:50 +0200

    fix path

--- misc/slack-notification/slack.sh
+++ misc/slack-notification/slack.sh
@@ -9,7 +9,7 @@ fi
 DEPLOYMENT_DETAILS="
 Environment Name: \`${ENV0_ENVIRONMENT_NAME}\`
 Revision: \`${REVISION}\`
-<https://app.env0.com//p/${ENV0_PROJECT_ID}/environments/${ENV0_ENVIRONMENT_ID}/deployments/${ENV0_DEPLOYMENT_LOG_ID}|View deployment>"
+<https://app.env0.com/p/${ENV0_PROJECT_ID}/environments/${ENV0_ENVIRONMENT_ID}/deployments/${ENV0_DEPLOYMENT_LOG_ID}|View deployment>"
 
 MESSAGE="${1}\n\n${DEPLOYMENT_DETAILS}"
 ICON_URL="https://uploads-ssl.webflow.com/5ceab5395d0f478e169de7c0/5f21eb54b57c7a8559c68ff7_Env0-Color%20Icon%403x.png"
