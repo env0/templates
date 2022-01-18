2d053f43a9f309c75953372a6e484c11e0a4c27e
Elbaz <eran.elbaz@env0.com>
2022-01-18 16:19:24 +0200

    change

--- misc/slack-notification/on-store-state.sh
+++ misc/slack-notification/on-store-state.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-diff <(terraform show -json .tf-plan | jq --sort-keys '.values') <(terraform show -json | jq --sort-keys '.planned_values')
+difference=diff <(terraform show -json .tf-plan | jq --sort-keys '.values') <(terraform show -json | jq --sort-keys '.planned_values')
+if (( ${#difference}==0 )); then
+  echo 'no change'
+else
+  echo 'change'
+fi
+
