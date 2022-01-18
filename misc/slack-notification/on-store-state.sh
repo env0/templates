581b0b04015c9088d9852dc8d230ca7fff508ca9
Elbaz <eran.elbaz@env0.com>
2022-01-18 16:31:33 +0200

    fix command

--- misc/slack-notification/on-store-state.sh
+++ misc/slack-notification/on-store-state.sh
@@ -2,8 +2,9 @@
 
 terraform show -json .tf-plan | jq --sort-keys '.values' > plan.json
 terraform show -json | jq --sort-keys '.planned_values' > state.json
-ls -l
-difference=diff plan.json state.json
+
+difference=$(diff plan.json state.json)
+
 if (( ${#difference}==0 )); then
   echo 'no change'
 else
