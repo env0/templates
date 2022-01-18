57b0f7ad1d283f56ee7976897d69f8f4996f4e04
Elbaz <eran.elbaz@env0.com>
2022-01-18 16:37:03 +0200

    fix

--- misc/slack-notification/on-terraform-plan.sh
+++ misc/slack-notification/on-terraform-plan.sh
@@ -4,8 +4,8 @@ terraform show -json .tf-plan
 
 terraform show -json
 
-terraform show -json .tf-plan | jq --sort-keys '.values' > plan.json
-terraform show -json | jq --sort-keys '.planned_values' > state.json
+terraform show -json .tf-plan | jq --sort-keys '.planned_values' > plan.json
+terraform show -json | jq --sort-keys '.values' > state.json
 
 difference=$(diff plan.json state.json)
 
