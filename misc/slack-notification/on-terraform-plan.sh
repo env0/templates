eb0dacf719b554d539184e09f681bc342a778918
Elbaz <eran.elbaz@env0.com>
2022-01-18 16:35:57 +0200

    log

--- misc/slack-notification/on-terraform-plan.sh
+++ misc/slack-notification/on-terraform-plan.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+terraform show -json .tf-plan
+
+terraform show -json
+
 terraform show -json .tf-plan | jq --sort-keys '.values' > plan.json
 terraform show -json | jq --sort-keys '.planned_values' > state.json
 
