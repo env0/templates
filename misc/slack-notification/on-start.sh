25be6a8365901796a928f89ae5f8a0f24e48bfb6
Elbaz <eran.elbaz@env0.com>
2022-01-19 12:10:25 +0200

    -f

--- misc/slack-notification/on-start.sh
+++ misc/slack-notification/on-start.sh
@@ -11,6 +11,4 @@ MESSAGE="${ACTION} running \`${ENV0_DEPLOYMENT_TYPE}\` deployment."
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${__dir}"/slack.sh "$MESSAGE"
 
-# Making sure we don't have the apply file as we need it to decide if we ran apply or not
-touch apply_ran_successfully
-rm apply_ran_successfully
+rm -f apply_ran_successfully
