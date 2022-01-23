245ae59d9551f52fb17c10fb8885fa2dd8d0b4b5
tomporat247 <tomporat247@gmail.com>
2022-01-23 15:14:21 +0200

    try with one equator

--- misc/slack-notification/on-plan.sh
+++ misc/slack-notification/on-plan.sh
@@ -4,7 +4,7 @@ terraform show -json | jq --sort-keys '.values' > state.json
 DIFFERENCE=$(diff plan.json state.json)
 echo "aa ${DIFFERENCE}"
 echo "bb ${#DIFFERENCE}"
-if (( ${#DIFFERENCE}==0 )); then
+if (( ${#DIFFERENCE}=0 )); then
   echo "No changes in terraform plan"
 else
   __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
