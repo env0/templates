90e8883f5d8924b22030d5bc11a7b97f9186a4ea
elbaz <eranelbaz97@gmail.com>
2022-01-24 14:21:47 +0200

    fix username

--- misc/slack-notification/slack.sh
+++ misc/slack-notification/slack.sh
@@ -14,6 +14,6 @@ Revision: \`${REVISION}\`
 MESSAGE="${1}\n\n${DEPLOYMENT_DETAILS}"
 ICON_URL="https://uploads-ssl.webflow.com/5ceab5395d0f478e169de7c0/5f21eb54b57c7a8559c68ff7_Env0-Color%20Icon%403x.png"
 
-PAYLOAD="{\"username\": \"webhookbot\", \"text\": \"${MESSAGE}\", \"icon_url\": \"${ICON_URL}\"}"
+PAYLOAD="{\"username\": \"env0\", \"text\": \"${MESSAGE}\", \"icon_url\": \"${ICON_URL}\"}"
 
 curl -X POST --data-urlencode "payload=${PAYLOAD}" ${SLACK_URL}
