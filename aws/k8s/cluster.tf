2eb24463404e3bf8650a6e2a6db4cd7c04614927
GiliFaroEnv0 <gili.faro@env0.com>
2022-01-31 13:44:57 +0200

    TEST1

--- aws/k8s/cluster.tf
+++ aws/k8s/cluster.tf
@@ -1,5 +1,5 @@
 module "my-cluster" {
-  source = "github.com/env0/k8s-modules//aws"
+  source = "github.com/env0/k8s-modules//aws?ref=feat-Do-not-use-AWS-CLI-command"
 
   region       = var.region
   cluster_name = var.cluster_name
