ea4ec97a3644fe02df60b8132852fbf58b119acc
Hever Farber <hever.farber@gmail.com>
2022-02-02 15:14:46 +0200

    Update cluster.tf

--- aws/k8s/cluster.tf
+++ aws/k8s/cluster.tf
@@ -1,5 +1,5 @@
 module "my-cluster" {
-  source = "github.com/env0/k8s-modules//aws"
+  source = "github.com/env0/k8s-modules//aws?ref=chore-check-csi-directory-permission"
 
   region       = var.region
   cluster_name = var.cluster_name
