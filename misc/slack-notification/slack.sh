a0b37a820767a64c26f5e5be1469cc71bdb329a9
Imri Zvik <imriz@users.noreply.github.com>
2022-02-03 11:12:22 +0200

    * Make Slack message format to be consistent with the default notifications format

--- misc/slack-notification/slack.sh
+++ misc/slack-notification/slack.sh
@@ -6,14 +6,11 @@ else
   REVISION=$ENV0_DEPLOYMENT_REVISION
 fi
 
-DEPLOYMENT_DETAILS="
-Environment Name: \`${ENV0_ENVIRONMENT_NAME}\`
-Revision: \`${REVISION}\`
-<https://app.env0.com/p/${ENV0_PROJECT_ID}/environments/${ENV0_ENVIRONMENT_ID}/deployments/${ENV0_DEPLOYMENT_LOG_ID}|View deployment>"
+DEPLOYMENT_DETAILS="<https://app.env0.com/p/${ENV0_PROJECT_ID}/environments/${ENV0_ENVIRONMENT_ID}/deployments/${ENV0_DEPLOYMENT_LOG_ID}|${ENV0_ENVIRONMENT_NAME}>"
 
-MESSAGE="${1}\n\n${DEPLOYMENT_DETAILS}"
+MESSAGE=":rocket: :information_source: ${DEPLOYMENT_DETAILS} - ${1}"
 ICON_URL="https://uploads-ssl.webflow.com/5ceab5395d0f478e169de7c0/5f21eb54b57c7a8559c68ff7_Env0-Color%20Icon%403x.png"
 
 PAYLOAD="{\"username\": \"env0\", \"text\": \"${MESSAGE}\", \"icon_url\": \"${ICON_URL}\"}"
 
-curl -X POST --data-urlencode "payload=${PAYLOAD}" ${SLACK_URL}
+curl -X POST --data-urlencode "payload=${PAYLOAD}" "${SLACK_URL}"
