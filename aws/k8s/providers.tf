249ce86a1f4cb98b4413ac5f2a5a61c4adaed812
Hever Farber <hever.farber@gmail.com>
2022-02-03 14:07:12 +0200

    Update providers.tf

--- aws/k8s/providers.tf
+++ aws/k8s/providers.tf
@@ -11,7 +11,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
-  host                   = module.my-cluster.kubernetes_host
+  host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
