70fb504f8e1cc7fd9eca457752ac9b79dd38f171
Liron sabo <lir48290@gmail.com>
2022-02-28 17:36:40 +0200

    Update main.tf

--- misc/aws_secret/datasource/main.tf
+++ misc/aws_secret/datasource/main.tf
@@ -20,6 +20,6 @@ data "aws_secretsmanager_secret" "secret" {
 
 resource "null_resource" "null" {
   triggers = {
-    cluster_instance_ids = join(",", data.aws_secretsmanager_secret.secret.tags)
+    cluster_instance_ids = join(",", data.aws_secretsmanager_secret.secret.tags[*].key)
   }
 }
