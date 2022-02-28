0c5fc876a8824f2626946621f815257badf4d634
Liron sabo <lir48290@gmail.com>
2022-02-28 15:32:18 +0200

    Update main.tf

--- misc/aws_secret/resource/main.tf
+++ misc/aws_secret/resource/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_secretsmanager_secret" "liron-example" {
   name = "liron-example-name"
 }
