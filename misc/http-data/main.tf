6ecebbb5b93398fceb7c72b38ac539e418cacaec
Liron sabo <lir48290@gmail.com>
2022-03-02 19:22:06 +0200

    Update main.tf

--- misc/http-data/main.tf
+++ misc/http-data/main.tf
@@ -1,7 +1,5 @@
 data "http" "example" {
   url = "https://blabla.free.beeceptor.com/my/api/path"
-  # url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
-  # url = "https://dummy-url-blabla-checkpoint-api.hashicorp.com/v1/check/terraform"
 
   # Optional request headers
   request_headers = {
