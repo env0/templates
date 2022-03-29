49820d9aa434d072aada4ecaeffeddcaaf27be95
Liron sabo <lir48290@gmail.com>
2022-03-29 13:38:33 +0300

    Update main.tf

--- bugs/too-big/main.tf
+++ bugs/too-big/main.tf
@@ -1,7 +1,16 @@
 resource "null_resource" "yoshhiig" {
-  count = 21900473
+  count = 21900
 }
 
-#resource "null_resource" "yosh2hiig" {
-#  count = 2147483647
-#}
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  count = 123000
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b[0].id
+  acl    = "private"
+}
