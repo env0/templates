0b7be492fb344570eea8af56da45e74416f95262
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-05-22 18:01:57 +0300

    Update main.tf

--- misc/custom-flow-tf-vars/main.tf
+++ misc/custom-flow-tf-vars/main.tf
@@ -19,5 +19,5 @@ output "custom_flow_tf_var_with_whitespace" {
 }
 
 resource "null_resource" "null" {
-  count = var.email == "default@domain.com" && var.user == "John Doe" ? 1 : "FAIL" ## asserts correct values
+ 
 }
