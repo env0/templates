707fac7d773f8dec9caf6743571d41c445e25c9a
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-05-22 15:40:25 +0300

    added user variable

--- misc/custom-flow-tf-vars/main.tf
+++ misc/custom-flow-tf-vars/main.tf
@@ -4,9 +4,17 @@ resource "null_resource" "null" {
 variable "email" {
   type = string
   description = "env variable exported in env0.yml"
-  default = "default@domain.com"
+}
+
+variable "user" {
+  type = string
+  description = "env variable that contains a whitespace in the value, exported in env0.yml"
 }
 
 output "custom_flow_tf_var" {
   value = var.email
+}
+
+output "custom_flow_tf_var_with_whitespace" {
+  value = var.user
 }
