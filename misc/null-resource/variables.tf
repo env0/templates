459b8cc761c1a8776e6ee6b96ce54f0b99d8a3db
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2022-06-20 10:39:06 +0300

    Create variables.tf

---
+++ misc/null-resource/variables.tf
@@ -0,0 +1,4 @@
+variable "organization" {
+  type    = list(any)
+  default = ["ecass", "ec-service-upg", "ec-service-impl", "ec-product", "ec-smart-ass", "ec-smart-product", "dwaas", "dw-service", "dw-product", "cloudplatform"]
+}
