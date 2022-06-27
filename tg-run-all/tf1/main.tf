d5813ad2354fa748a836be065677fb3486fb2ce6
tomer <tomer.landesman@env0.com>
2022-06-27 15:48:44 +0300

    add

--- tg-run-all/tf1/main.tf
+++ tg-run-all/tf1/main.tf
@@ -1,3 +1,7 @@
 resource "null_resource" "tomer1" {
     count = 1
 }
+
+output "tf1_output" {
+  value = var.tomer
+}
--- tg-run-all/tf2/main.tf
+++ tg-run-all/tf2/main.tf
@@ -1,3 +1,7 @@
 resource "null_resource" "tomer3" {
     count = 2
 }
+
+output "tf2_output" {
+  value = var.tomer
+}
