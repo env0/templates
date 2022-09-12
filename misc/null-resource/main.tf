9cfe353738252d57bd2d44e5bad452a0629ecbe1
samuel-br <shmuelbrener@gmail.com>
2022-09-12 12:53:29 +0300

    empty

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,2 +1,22 @@
 resource "null_resource" "null" {
 }
+terraform {
+  required_providers {
+    aws        = "~> 3.70.0"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "random_string" "random" {
+  length = "16"
+  special = "false"
+  min_lower = "16"
+}
+
+resource "aws_s3_bucket" "yaron" {
+  bucket = "yaron-tessst-${random_string.random.result}"
+  force_destroy = true
+}
