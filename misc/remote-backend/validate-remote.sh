28dbae993113a3867146a8489f8fc2e6d9db2504
tomer <tomer.landesman@env0.com>
2022-09-17 15:36:32 +0300

    added validation script

---
+++ misc/remote-backend/validate-remote.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+EXPECTED_TYPE="remote"
+ACTUAL_TYPE= cat .terraform/terraform.tfstate | jq '.backend.type'
+if [[ "$EXPECTED_TYPE" == "$ACTUAL_TYPE" ]];
+then
+  echo 'backend type is remote. OK'
+  exit 0
+else
+  echo 'backend type is not remote. FAILED'
+  exit 1
+fi
