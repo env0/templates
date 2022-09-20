47eb1b51d87d8632cf71ea2707653467f8971f25
ChaimP <chaim.platonov@env0.com>
2022-09-20 19:39:20 +0300

    more versions and tests

--- misc/ensure-permissions-on-tools-and-folders-for-user-node/ensure-permissions.sh
+++ misc/ensure-permissions-on-tools-and-folders-for-user-node/ensure-permissions.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
-set -e
+set -e -x
 
-# Check tools permissions
+# list installed apk packages
+apk list --installed | sort
+
+# list installed python packages
+pip3 list | sort
+
+# from base image
 node --version
 npm --version
 yarn --version
+
+#from alpine repos
+bash --version
 git --version
-python --version
 python3 --version
-pip --version
 pip3 --version
+jq --version
+openssl version
+ssh -V
+curl --version
+
+# other
 aws --version
+awsv2 --version
 gcloud --version
 az --version
 kubectl version --client
@@ -18,9 +32,7 @@ tfenv --version
 terraform --version
 tgenv --version
 terratag --help
-jq --version
 pwsh -v
-curl --version
 infracost --version
 
 # Check write permissions
@@ -34,4 +46,12 @@ mkdir -p /home/node/.cache
 python3 -m venv -h
 
 # Check packages install
-pip install --user j2cli
+pip install --user ansible
+ansible --version
+
+apk update
+apk add rsync
+rsync --version
+
+node install -g @env0/cli
+env0 help
