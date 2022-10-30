322207ffdf09211ff8780680fca5cdf6a92ebfc4
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2022-10-30 15:54:55 +0200

    Update variables.tf

--- misc/null-resource/variables.tf
+++ misc/null-resource/variables.tf
@@ -2,3 +2,5 @@ variable "organization" {
   type    = list(any)
   default = ["ecass", "ec-service-upg", "ec-service-impl", "ec-product", "ec-smart-ass", "ec-smart-product", "dwaas", "dw-service", "dw-product", "cloudplatform"]
 }
+
+
