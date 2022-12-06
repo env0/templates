9625794c379c57e98cb4b11ba71dfd0a0729bda0
avner.sorek <avner.sorek@env0.com>
2022-12-06 18:11:17 +0200

    more auth

--- aws/vault-server-on-eks/providers.tf
+++ aws/vault-server-on-eks/providers.tf
@@ -18,6 +18,10 @@ data "aws_eks_cluster" "cluster" {
   name = var.cluster_name
 }
 
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = var.cluster_name
+}
+
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
@@ -33,5 +37,6 @@ provider "helm" {
 provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
   load_config_file       = false
 }
