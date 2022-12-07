eb1c73604f3db7f206c6b07fe2f6828f52b52915
avner.sorek <avner.sorek@env0.com>
2022-12-07 17:21:47 +0200

    init postgres and some more things

--- aws/vault-server-on-eks/main.tf
+++ aws/vault-server-on-eks/main.tf
@@ -1,3 +1,14 @@
+# Postgres Storgae Backend
+data "kubectl_file_documents" "postgres_manifest" {
+  content = file("postgres.yaml")
+}
+
+resource "kubectl_manifest" "postgres" {
+  for_each  = data.kubectl_file_documents.postgres_manifest.manifests
+  yaml_body = each.value
+}
+
+# Vault chart
 
 resource "helm_release" "vault_release" {
   name             = "self-hosted-vault"
@@ -6,58 +17,7 @@ resource "helm_release" "vault_release" {
   chart            = "vault"
   create_namespace = true
   version          = "0.21.0"
+  values = ["${file("vault-config.yaml")}"]
 
-  // Configuration reference in
-  // https://www.vaultproject.io/docs/platform/k8s/helm/configuration
-  values = [
-    "${file("vault-config.yaml")}"
-  ]
-
-  # These additonal settings are merged with the values defined above
-  # This is done to hide the postgresql database connection info
-  # The secret is owned by the webgen3 devops team
-
-  set {
-    name  = "server.volumes[0].name"
-    value = "userconfig-vault-storage-config"
-  }
-
-  set {
-    name  = "server.volumes[0].secret.defaultMode"
-    value = "420"
-  }
-
-  set {
-    name  = "server.volumes[0].secret.secretName"
-    value = "vault-storage-config"
-  }
-
-  set {
-    name  = "server.volumeMounts[0].mountPath"
-    value = "/vault/userconfig/vault-storage-config"
-  }
-
-  set {
-    name  = "server.volumeMounts[0].name"
-    value = "userconfig-vault-storage-config"
-  }
-
-  set {
-    name  = "server.volumeMounts[0].readOnly"
-    value = "true"
-  }
-
-  set {
-    name  = "server.extraArgs"
-    value = "-config=/vault/userconfig/vault-storage-config/config.hcl"
-  }
-}
-
-data "kubectl_file_documents" "postgres_manifest" {
-  content = file("postgres.yaml")
-}
-
-resource "kubectl_manifest" "postgres" {
-  for_each  = data.kubectl_file_documents.postgres_manifest.manifests
-  yaml_body = each.value
+  depends_on = [kubectl_manifest.postgres]
 }
