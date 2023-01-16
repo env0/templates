ccedf10c775bc7557f1ca584c38c7d04e4e0a797
Tomer Landesman <tomer.landesman@env0.com>
2023-01-16 11:23:04 +0200

    apply check

--- misc/remote-plan/run-apply.sh
+++ misc/remote-plan/run-apply.sh
@@ -4,20 +4,21 @@ APPLY_OUT_FILE="apply-out.txt"
 
 function checkApplyOutput () {
   status=$1
+  echo "status $status"
   if [ "$status" -eq "0" ]; then
-    echo "Apply succeeded instead of failing"
+    echo "Apply succeeding instead of failing"
     exit 1
   fi
   ERRORS=`grep -c 'Apply not allowed for workspaces with a VCS connection' $APPLY_OUT_FILE`
-  if [ "$ERRORS" -eq "0" ]; then echo "plan failed" 1>&2 && exit 1; fi
+  if [ "$ERRORS" -eq "0" ]; then echo "Apply failed fo a reason other than VCS" 1>&2 && exit 1; fi
 }
 
-echo "test apply failure"
+echo "testing apply failure"
 status="0"
 terraform -chdir='./local-run' apply 2> $APPLY_OUT_FILE || status="1"
 checkApplyOutput $status
 
-echo "test destroy failure"
+echo "testing destroy failure"
 status="0"
 terraform -chdir='./local-run' destroy 2> $APPLY_OUT_FILE || status="1"
 checkApplyOutput $status
