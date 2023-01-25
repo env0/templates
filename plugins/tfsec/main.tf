7836e0fa00583f059b551633c1ab8f4ca0e7428a
lironsabo <lir48290@gmail.com>
2023-01-25 10:52:48 +0200

    Make the plugin templates more "valid" according to TFLinted

--- plugins/tfsec/main.tf
+++ plugins/tfsec/main.tf
@@ -1,5 +1,9 @@
 terraform {
-  required_version = ">= 1.2.2"
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    null        = "~> 3.2.1"
+  }
 }
 
 resource "null_resource" "null_resource" {
