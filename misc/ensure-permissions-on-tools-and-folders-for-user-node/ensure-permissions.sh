e747f87799aa2b60e9264e423750d137c7fff4b5
Chaim Platonov <chaim.platonov@env0.com>
2023-05-07 11:58:18 +0300

    Chore - helm permissions check (#177)
    helm permissions check

--- misc/ensure-permissions-on-tools-and-folders-for-user-node/ensure-permissions.sh
+++ misc/ensure-permissions-on-tools-and-folders-for-user-node/ensure-permissions.sh
@@ -34,6 +34,8 @@ tgenv --version
 terratag --help
 pwsh -v
 infracost --version
+helm version
+helm diff version
 
 # Check write permissions
 touch /opt/test
@@ -44,6 +46,9 @@ touch /home/node/test
 rm /home/node/test
 mkdir -p /home/node/.cache
 python3 -m venv -h
+helm repo add env0 https://env0.github.io/self-hosted
+helm repo update
+helm show chart env0/env0-agent
 
 # Check packages install
 pip install --user ansible
