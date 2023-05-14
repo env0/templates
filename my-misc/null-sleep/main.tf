8c78329a68d03f84516374723e1d5a4018a736c4
alonnoga <alon.noga@env0.com>
2023-05-14 18:52:09 +0300

    added null sleep

---
+++ my-misc/null-sleep/main.tf
@@ -0,0 +1,9 @@
+resource "time_sleep" "wait_30_seconds" {
+
+  create_duration = "8s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "null" {
+  depends_on = [time_sleep.wait_30_seconds]
+}
