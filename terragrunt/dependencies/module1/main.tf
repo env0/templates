df2feb0896041f36d6d181165f5cfcab93981f5e
Hever Farber <hever.farber@gmail.com>
2023-05-18 16:18:59 +0300

    add-error

---
+++ terragrunt/dependencies/module1/main.tf
@@ -0,0 +1,12 @@
+locals {
+  compare     = "success"
+  testSuccess = "success"
+  testFail    = "fail"
+}
+
+module "errorcheck_invalid" {
+  source = "rhythmictech/errorcheck/terraform"
+
+  assert = local.compare == local.testFail
+  error_message = "Your assertion is not valid"
+}
