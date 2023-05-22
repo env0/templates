07394323f5b5ad7e11e02ba01736d47bdfab6188
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-05-22 14:29:29 +0300

    Update main.tf

--- misc/failed-template/main.tf
+++ misc/failed-template/main.tf
@@ -1,11 +1,13 @@
-
+# tf file with error
 resource "helm_release" "heml" {
  name = "my-helm-release"
  values = [
-   "image",
-   "repository: nginx",
-   "tag: 1.16.0",
-   "pullPolicy: IfNotPresent"
+ <<EOF
+ image
+ repository: nginx
+ tag: 1.16.0
+ pullPolicy: IfNotPresent
+ EOF
  ]
  chart = "./myetc.tar.gz"
 }
