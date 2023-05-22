7819d18f01ae6a0e2eec5af5ef78f3a0b22fa12e
RLRabinowitz <rlrabinowitz2@gmail.com>
2023-05-22 11:27:14 +0000

    "Update the file"

--- misc/failed-template/main.tf
+++ misc/failed-template/main.tf
@@ -1,12 +1,11 @@
 
-# fixed code
 resource "helm_release" "heml" {
  name = "my-helm-release"
- values = <<-EOF
- image
-   repository: nginx
-   tag: 1.16.0
-   pullPolicy: IfNotPresent
- EOF
+ values = [
+   "image",
+   "repository: nginx",
+   "tag: 1.16.0",
+   "pullPolicy: IfNotPresent"
+ ]
  chart = "./myetc.tar.gz"
 }
