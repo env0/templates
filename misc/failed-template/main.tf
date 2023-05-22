9c7f65ca27eb9ebe638924a59202d61419294cca
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-05-22 16:37:41 +0300

    Update main.tf

--- misc/failed-template/main.tf
+++ misc/failed-template/main.tf
@@ -1,11 +1,13 @@
 
 resource "helm_release" "heml" {
  name = "my-helm-release"
- values = <<EOF
+ values = [
+  <<EOF
  image:
    repository: nginx
    tag: 1.16.0
    pullPolicy: IfNotPresent
  EOF
+ ]
  chart = "./myetc.tar.gz"
 }
