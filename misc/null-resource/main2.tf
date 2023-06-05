3744dfef0a9f1d50ebda9a9269fd3a46c317dc51
Sagy Drucker <sag@sagys-mbp.lan>
2023-06-05 10:40:46 +0300

    example

--- misc/null-resource/main2.tf
+++ misc/null-resource/main2.tf
@@ -1,2 +1,14 @@
-resource "null_resource" "null" {
+resource "null_resource" "example" {
+  triggers = {
+    timestamp = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'Hello, Terraform!'"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo 'Terraform is destroying the resources.'"
+  }
 }
