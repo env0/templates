246ed18929617183e99ade487c9514590259a810
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-07-16 11:17:23 +0300

    Update ec2.tf

--- aws/ec2/ec2.tf
+++ aws/ec2/ec2.tf
@@ -1,11 +1,8 @@
-variable "aws_default_region" { default = "us-east-1"}
-variable "instance_type" { default = "t2.small"}
-
 provider "aws" {
-  region = var.aws_default_region
+  region = "us-east-1"
 }
 
 resource "aws_instance" "test-instance" {
   ami = "ami-0c654c3ab463d22f6"
-  instance_type = var.instance_type
+  instance_type = "t2.micro"
 }
