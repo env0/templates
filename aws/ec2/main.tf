ce7821715a6aee644022408af17ffb6bdb6c336f
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-07-16 11:33:44 +0300

    Update main.tf

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -2,7 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "random_uuid" "uuid" { }
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "redash-key-pair" {
+  key_name   = "redash-key-${random_uuid.uuid.result}"
+  public_key = tls_private_key.private_key.public_key_openssh
+}
+
 resource "aws_instance" "test-instance" {
   ami = "ami-0c654c3ab463d22f6"
   instance_type = "t2.micro"
+  key_name = aws_key_pair.redash-key-pair.key_name
 }
