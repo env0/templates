ff920e6fc3a85036098a92548e0c05b80b1022f1
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-07-20 13:08:20 +0300

    Update main.tf

--- gcp/computed-engine/main.tf
+++ gcp/computed-engine/main.tf
@@ -7,10 +7,6 @@ provider "google" {
     zone = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-    name = "terraform-network"
-}
-
 resource "google_compute_instance" "vm_instance" {
     name = "terraform-instance2"
     machine_type = "f1-micro"
@@ -18,12 +14,6 @@ resource "google_compute_instance" "vm_instance" {
     boot_disk {
         initialize_params {
             image = "centos-cloud/centos-7"
-        }
-    }
-
-    network_interface {
-        network = google_compute_network.vpc_network.name
-        access_config {
         }
     }
 }
