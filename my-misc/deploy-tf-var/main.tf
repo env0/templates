f1f91102ad37050b8694c724a92644951a30e39c
alonnoga <alon.noga@env0.com>
2023-07-26 16:07:26 +0300

    change

--- my-misc/deploy-tf-var/main.tf
+++ my-misc/deploy-tf-var/main.tf
@@ -1,11 +1,4 @@
-
 resource "null_resource" "null" {
-}
-
-variable "alon" {
-  type = string
-  description = "env variable exported from deployment"
-  default = "default@domain.com"
 }
 
 output "custom_flow_tf_var" {
