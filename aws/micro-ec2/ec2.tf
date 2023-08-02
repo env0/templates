652329101912e60330ddd3a9a72b1b2355a4b6ca
sag <sagy.drucker@env0.com>
2023-08-02 12:55:04 +0300

    region

--- aws/micro-ec2/ec2.tf
+++ aws/micro-ec2/ec2.tf
@@ -1,7 +1,10 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
 resource "aws_instance" "example_server" {
   ami           = "ami-04e914639d0cca79a"
   instance_type = "t2.micro"
-  region  = "us-east-1"
 
   tags = {
     Name = "Sag"
