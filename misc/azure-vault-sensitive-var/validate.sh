d66bd018847f2139e2ed3080097e340a23c3c6c1
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-09-05 12:38:10 +0300

    fix validate.sh

--- misc/azure-vault-sensitive-var/validate.sh
+++ misc/azure-vault-sensitive-var/validate.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
-if [[ "$SHAG_FAKE_SECRET" == "FAKE_NEWS" ]];
+if [[ "$AZURE_SHAG_FAKE_SECRET" == "FAKE_NEWS" ]];
 then
   echo "Loaded secret from azure vault successfully"
   exit 0
 else
-  echo "Validation failed! Value of azure vault secret is $SHAG_FAKE_SECRET"
+  echo "Validation failed! Value of azure vault secret is $AZURE_SHAG_FAKE_SECRET"
   exit 1
 fi
