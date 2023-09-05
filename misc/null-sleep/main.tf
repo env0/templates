2634e96944c729e016df79898294a529806fe429
Alon Noga <54034642+alonnoga@users.noreply.github.com>
2023-09-05 15:43:27 +0300

    Update main.tf

--- misc/null-sleep/main.tf
+++ misc/null-sleep/main.tf
@@ -1,2 +1,13 @@
-resource "null_resource" "null" {
+# This resource will destroy (potentially immediately) after null_resource.next
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "300s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
 }
