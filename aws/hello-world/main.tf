509a842b0920ca26959712baffd48cd9d1323e5e
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-09-11 10:44:25 +0300

    Update main.tf

--- aws/hello-world/main.tf
+++ aws/hello-world/main.tf
@@ -1,11 +1,6 @@
 provider "aws" {
   version = "~> 4.0"
   region  = "us-east-1"
-  assume_role_with_web_identity {
-    role_arn                = "arn:aws:iam::170412844252:role/yossi-test-oidc-role"
-    session_name            = "env0_OIDC_session"
-    web_identity_token_file = "web-identity-token.txt"
-  }
 }
 
 terraform {
