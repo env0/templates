5f35c4f0dee3d28f01fa7d307878ec43e827e0f9
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-09-18 17:31:53 +0300

    Update test-vault.sh

--- misc/vault-integration/test-vault.sh
+++ misc/vault-integration/test-vault.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$VAULT_ADDR" || -z "$VAULT_NAMESPACE" || -z "$VAULT_ROLE" ]]; then
-    echo "MISSING REQUIRED VARIABLES: VAULT_ROLE, VAULT_NAMESPACE, VAULT_ADDR"
-    exit 1
-fi
-
-
 echo "Logging in to Vault"
 
 export VAULT_TOKEN=$(./vault write auth/env0-jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}" -format=json | jq --raw-output '.auth.client_token')
