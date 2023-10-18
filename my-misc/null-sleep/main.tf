c4c67bbe185996fc70acb8a4daa72a6fa4ef26dd
Alon Noga <54034642+alonnoga@users.noreply.github.com>
2023-10-18 17:30:32 +0300

    Update main.tf

--- my-misc/null-sleep/main.tf
+++ my-misc/null-sleep/main.tf
@@ -1,6 +1,6 @@
 resource "time_sleep" "wait_30_seconds" {
 
-  create_duration = "8s"
+  create_duration = "120s"
 }
 
 # This resource will create (at least) 30 seconds after null_resource.previous
