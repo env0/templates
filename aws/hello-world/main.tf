bdaba166ba89c887858c0f6591fce540fcc7ea4f
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-10-18 13:04:13 +0300

    Update main.tf

--- aws/hello-world/main.tf
+++ aws/hello-world/main.tf
@@ -13,9 +13,9 @@ provider "aws" {
   version = "~> 4.0"
   region  = "us-east-1"
 
-  assume_role_with_web_identity {
-    role_arn                = "arn:aws:iam::170412844252:role/yossi-test-oidc-aws"
-    session_name            = "env0_OIDC_session"
-    web_identity_token_file = "web-identity-token.txt"
+  #assume_role_with_web_identity {
+  #  role_arn                = "arn:aws:iam::170412844252:role/yossi-test-oidc-aws"
+  #  session_name            = "env0_OIDC_session"
+  #  web_identity_token_file = "web-identity-token.txt"
   }
 }
