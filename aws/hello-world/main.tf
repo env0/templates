f049b733d4efa9072632ed6b52aa430bc9126a9c
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-10-31 15:33:51 +0200

    Update main.tf

--- aws/hello-world/main.tf
+++ aws/hello-world/main.tf
@@ -10,7 +10,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   assume_role_with_web_identity {
-    # role_arn                = "arn:aws:iam::170412844252:role/built-in-oidc-test-role"
+    role_arn                = "arn:aws:iam::170412844252:role/built-in-oidc-test-role"
     session_name            = "env0_OIDC_session"
     web_identity_token_file = "web-identity-token.txt"
   }
