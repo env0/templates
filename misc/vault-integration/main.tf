c475c0b2ea1dbeb3adac8cdb002b979b5b9022e2
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-10-31 16:55:26 +0200

    Update main.tf

--- misc/vault-integration/main.tf
+++ misc/vault-integration/main.tf
@@ -1,10 +1,10 @@
-#provider "vault" {
-#  address = "http://ab2fe5d8c6c3642cf893184fc93d6be7-1541439417.us-east-1.elb.amazonaws.com:8200/"
-#  auth_login_jwt {
-#    namespace = "admin"
-#    role = "bors-env0-integration-tests-role"
-#  }
-#}
+provider "vault" {
+  address = "http://ab2fe5d8c6c3642cf893184fc93d6be7-1541439417.us-east-1.elb.amazonaws.com:8200/"
+  auth_login_jwt {
+    namespace = "admin"
+    role = "bors-env0-integration-tests-role"
+  }
+}
 
 resource "null_resource" "null" {
 }
