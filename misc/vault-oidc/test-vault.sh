e9a5517b787a9eea35e9aa7ba3bd226106c657dd
Yossi Kerner <yossi.kerner@env0.com>
2023-11-02 17:23:46 +0200

    add echos

--- misc/vault-oidc/test-vault.sh
+++ misc/vault-oidc/test-vault.sh
@@ -2,6 +2,7 @@
 set -e
 
 ./vault write auth/jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}"
-echo "Running some Vault commands"
+echo "Putting vault key value"
 ./vault kv put -mount=oidc-test creds passcode=my-long-passcode
+echo "Getting vault key value"
 ./vault kv get -mount=oidc-test -field=passcode creds
