d4bca7e1f01556d635337d5528fc70dd2c453560
Yossi Kerner <yossi.kerner@env0.com>
2023-11-02 16:19:43 +0200

    remove duplicate code

--- misc/vault-oidc/test-vault.sh
+++ misc/vault-oidc/test-vault.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Installing vault client..."
-
-curl -sL https://releases.hashicorp.com/vault/1.11.1/vault_1.11.1_linux_amd64.zip -o vault1.zip
-unzip -o vault1.zip
-./vault --version
-
-echo "Logging in to Vault"
-
-export VAULT_TOKEN=$(./vault write auth/jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}" -format=json | jq --raw-output '.auth.client_token')
-
 echo "Running some Vault commands"
 
 ./vault kv put -mount=secrets-for-env0 creds passcode=my-long-passcode
