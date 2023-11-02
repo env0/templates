03de86b63582380a06cba3d3f7a8d460777c59e6
Yossi Kerner <yossi.kerner@env0.com>
2023-11-02 18:15:15 +0200

    add more configurations

--- misc/vault-oidc/configure-vault.sh
+++ misc/vault-oidc/configure-vault.sh
@@ -5,11 +5,28 @@ set -e
 export VAULT_TOKEN=$VAULT_DEV_ROOT_TOKEN
 
 echo Configuring vault
+./vault secrets disable secrets-for-env0/ || true
 ./vault secrets enable -path=secrets-for-env0/ kv
 
 # Bound a ACL policy for permissions
 ./vault policy write env0-access - <<EOF
 path "secrets-for-env0/*" {
     capabilities = ["read", "create", "update"]
+}
+EOF
+
+./vault write auth/env0-jwt/role/$VAULT_ROLE - <<EOF
+{
+  "user_claim": "sub",
+  "role_type": "jwt",
+  "bound_audiences": [
+    "https://dev.env0.com",
+    "https://dev-env0.auth0.com/userinfo"
+  ],
+  "bound_claims": {
+    "https://env0.com/organization": "$ENV0_ORGANIZATION_ID",
+    "https://env0.com/apiKeyType": "oidc"
+  },
+  "policies": ["env0-access"]
 }
 EOF
