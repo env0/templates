cf8dbaad2ef4d6bc88ae48122fbbc2a65baee2a6
Yossi Kerner <yossi.kerner@env0.com>
2023-11-02 16:00:48 +0200

    add custom file and downlaod vault

--- misc/vault-oidc/test-vault.sh
+++ misc/vault-oidc/test-vault.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "Installing vault client..."
+
+curl -sL https://releases.hashicorp.com/vault/1.11.1/vault_1.11.1_linux_amd64.zip -o vault1.zip
+unzip -o vault1.zip
+./vault --version
+
 echo "Logging in to Vault"
 
 export VAULT_TOKEN=$(./vault write auth/jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}" -format=json | jq --raw-output '.auth.client_token')
