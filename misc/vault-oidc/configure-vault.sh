55cb34320d6824f0aa313f182a46ff1040aa36d9
Yossi Kerner <yossi.kerner@env0.com>
2023-11-02 18:28:55 +0200

    use var in configure

--- misc/vault-oidc/configure-vault.sh
+++ misc/vault-oidc/configure-vault.sh
@@ -15,7 +15,7 @@ path "secrets-for-env0/*" {
 }
 EOF
 
-./vault write auth/jwt/role/$VAULT_ROLE - <<EOF
+./vault write auth/$VAULT_JWT_AUTH_BACKEND_PATH/role/$VAULT_ROLE - <<EOF
 {
   "user_claim": "sub",
   "role_type": "jwt",
