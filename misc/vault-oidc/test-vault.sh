85d29bb82343f7d0ee6537e250e440c68aad8197
Yossi Kerner <yossi.kerner@env0.com>
2023-11-02 18:19:16 +0200

    use the VAULT_TOKEN

--- misc/vault-oidc/test-vault.sh
+++ misc/vault-oidc/test-vault.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-./vault write auth/jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}"
+export VAULT_TOKEN=$(./vault write auth/env0-jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}" -format=json | jq --raw-output '.auth.client_token')
+
 echo "Putting vault key value"
 ./vault kv put -mount=secrets-for-env0 creds passcode=my-long-passcode
 echo "Getting vault key value"
