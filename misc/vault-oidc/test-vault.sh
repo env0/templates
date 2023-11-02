2cd8fa0c4ef1b01e2dd0af960312e5e5fd5cb742
Yossi Kerner <yossi.kerner@env0.com>
2023-11-02 18:27:10 +0200

    add path var

--- misc/vault-oidc/test-vault.sh
+++ misc/vault-oidc/test-vault.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export VAULT_TOKEN=$(./vault write auth/env0-jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}" -format=json | jq --raw-output '.auth.client_token')
+export VAULT_TOKEN=$(./vault write auth/${VAULT_JWT_AUTH_BACKEND_PATH}/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}" -format=json | jq --raw-output '.auth.client_token')
 
 echo "Putting vault key value"
 ./vault kv put -mount=secrets-for-env0 creds passcode=my-long-passcode
