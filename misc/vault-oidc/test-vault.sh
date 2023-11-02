0e26e906571a64ece476e1d531f9c2aee72ab9ef
Yossi Kerner <yossi.kerner@env0.com>
2023-11-02 16:26:27 +0200

    test changes

--- misc/vault-oidc/test-vault.sh
+++ misc/vault-oidc/test-vault.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "Logging in to Vault"
+
+export VAULT_TOKEN=$(./vault write auth/jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}" -format=json | jq --raw-output '.auth.client_token')
+
 echo "Running some Vault commands"
 
 ./vault kv put -mount=secrets-for-env0 creds passcode=my-long-passcode
