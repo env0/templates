e28aaa4e58b066f2260eb5358fef5811455f4d0b
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2023-11-05 10:43:27 +0200

    Update main.tf

--- aws/hello-world/main.tf
+++ aws/hello-world/main.tf
@@ -1,17 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+provider "aws" {
+  version = "~> 4.0"
+  region  = "us-east-1"
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  assume_role_with_web_identity {
-    role_arn                = "arn:aws:iam::170412844252:role/built-in-oidc-test-role"
-    session_name            = "env0_OIDC_session"
-    web_identity_token_file = "web-identity-token.txt"
-  }
+terraform {
+  required_version = ">= 0.13"
 }
