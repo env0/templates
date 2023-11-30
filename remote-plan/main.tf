4dfd8a4e42ace477a3d8a941f8f72c3facc4020d
Tomer Landesman <tomer.landesman@env0.com>
2023-11-30 11:37:08 +0200

    s

--- remote-plan/main.tf
+++ remote-plan/main.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    # hostname     = "backend-pr13908.api.dev.env0.com"
-    # organization = "f5f63028-62dc-4273-910a-2f4edb310896.81609940-4103-4817-bb6a-23031487f832"
-    hostname     = "backend-dev.api.dev.env0.com"
-    organization = "bd12a04a-b051-4f51-9a8a-8a32ac198802.6b1b0897-5322-48cd-98b7-5e11766f54c8"
+    hostname     = "backend-pr13949.api.dev.env0.com"
+    organization = "87313781-6b8b-4ed3-9f26-fcf5096e5e62.1827e489-3b42-4ee9-928e-f430135c0108"
+    # hostname     = "backend-dev.api.dev.env0.com"
+    # organization = "bd12a04a-b051-4f51-9a8a-8a32ac198802.6b1b0897-5322-48cd-98b7-5e11766f54c8"
     # hostname     = "backend.api.env0.com"
     # organization = "e3900e8f-002c-4689-8004-bbab8f269ab9.b9d0773a-96c7-4d67-be3d-2b2521381d92"
     # organization = "tomer123"
