6dafcba06a069b81b4081019bfa4cf4dcec0bbd6
Alon Noga <54034642+alonnoga@users.noreply.github.com>
2023-11-30 12:25:32 +0200

    Update main.tf

--- mine/s3-bucket/main.tf
+++ mine/s3-bucket/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 resource "aws_s3_bucket" "example" {
   bucket = "alon-tf-bucket"
 }
