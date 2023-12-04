c04c174f49460cb1e624397d254fcacae0028497
yarivg <yarivgavriel2@gmail.com>
2023-12-04 11:38:46 +0200

    add my ec2

---
+++ aws/my-ec2/main.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "latest_amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "pr13901-yariv-testing-stuff" {
+  ami           = data.aws_ami.latest_amazon_linux_2.id
+  instance_type = "t2.micro"
+  key_name      = "pr13901-yariv"
+
+  tags = {
+    Name = "pr13901-yariv"
+  }
+}
