6f77d99c96d1de950e720f0da53a9799de807138
Yossi Kerner <yossi.kerner@env0.com>
2023-12-18 13:13:34 +0200

    add many environments project template

---
+++ misc/project-with-many-envs/main.tf
@@ -0,0 +1,35 @@
+variable "env0_api_key" { }
+
+variable "env0_api_secret" { }
+
+variable "environments_amount" { }
+
+terraform {
+  required_providers {
+    env0 = {
+      source = "env0/env0"
+    }
+  }
+}
+
+provider "env0" {
+  api_key = var.env0_api_key
+  api_secret = var.env0_api_secret
+}
+
+data "env0_project" "provider_project" {
+  name = "Provider Project"
+}
+
+resource "env0_template" "null_template" {
+  name        = "null template provider"
+  repository  = "https://github.com/env0/templates"
+  path        = "misc/null-resource"
+}
+
+resource "env0_environment" "example" {
+	count = var.environments_amount;
+  name        = "environment"
+  project_id  = data.env0_project.provider_project.id
+  template_id = data.env0_template.null_template.id
+}
