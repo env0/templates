d70ac6051ade5606a4d780a7d73d99dfe775b976
Tomer Landesman <tomer.landesman@env0.com>
2023-12-20 15:04:00 +0200

    s

--- remote-plan/main.tf
+++ remote-plan/main.tf
@@ -1,14 +1,9 @@
 terraform {
   cloud {
-    # hostname     = "backend-pr14022.api.dev.env0.com"
-    # organization = "be029f7d-9089-4d20-8c7d-c04f54ff6149.5df4877e-1ab8-4ea4-9a44-2f30efec9a0a"
     hostname     = "backend-dev.api.dev.env0.com"
     organization = "bd12a04a-b051-4f51-9a8a-8a32ac198802.6b1b0897-5322-48cd-98b7-5e11766f54c8"
-    # hostname     = "backend.api.env0.com"
-    # organization = "e3900e8f-002c-4689-8004-bbab8f269ab9.b9d0773a-96c7-4d67-be3d-2b2521381d92"
-    # organization = "tomer123"
     workspaces {
-      name = "qa-party"
+      name = "product-demo"
     }
   }
 
@@ -17,7 +12,7 @@ terraform {
 }
 
 resource "null_resource" "null" {
-  count = 3
+  count = 5
 }
 # provider "aws" {
 #   region = "us-west-2"
--- terragrunt/misc/module-resource/module/null-module/main.tf
+++ terragrunt/misc/module-resource/module/null-module/main.tf
@@ -1,9 +1,13 @@
+terraform {
+  backend "remote" {
+    hostname     = "backend-dev.api.dev.env0.com"
+    organization = "bd12a04a-b051-4f51-9a8a-8a32ac198802.6b1b0897-5322-48cd-98b7-5e11766f54c8"
+    workspaces {
+      name = "env05e894e"
+    }
+  }
+}
+
 resource "null_resource" "null" {
 }
 
-output "my complex output" {
-  value = {
-    foo = "bar"
-    baz = "qux"
-  }
-}
