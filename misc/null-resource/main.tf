598ec9a4a8445ee8d9a4bbab89d56f70e0261907
Alon Noga <54034642+alonnoga@users.noreply.github.com>
2023-12-26 13:55:23 +0200

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,8 +1,9 @@
 resource "null_resource" "null" {
 }
-resource "null_resource" "null1" {
-}
 resource "null_resource" "null2" {
 }
 resource "null_resource" "null3" {
 }
+resource "null_resource" "null4" {
+}
+
