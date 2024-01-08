0b74c8fa4dcede7c212c0d028c74c22805b0d5ba
yarivg <yarivgavriel2@gmail.com>
2024-01-08 14:30:39 +0200

    pref/5.0.0-alpha.1 - change source - no pref

--- misc/null-sleep/main.tf
+++ misc/null-sleep/main.tf
@@ -3,5 +3,5 @@ resource "null_resource" "null" {
 
 module "aaa3" {
   source  = "api-pr14408.dev.env0.com/2db1209f-7646-46b6-aafc-b7c9f34ea9f6/templates-test/templatestest"
-  version = "pref/5.0.0-alpha.1"
+  version = "5.0.0-alpha.1"
 }
