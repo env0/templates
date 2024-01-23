bffc5ddecc96ad42dc200c7d056663157a112726
yarivg <yarivgavriel2@gmail.com>
2024-01-23 22:57:49 +0200

    try with files and long resource

--- misc/null-for-each/main.tf
+++ misc/null-for-each/main.tf
@@ -1,3 +1,17 @@
 resource "null_resource" "null2" {
-  for_each = toset(["aaa.yariv", "bbb", "ccc"])
+  for_each = toset(["aaa.yarivvvvvvvvvvvvvvvv.trying.to.do.a.very.long.resource.maybe.it.will.reproduce", "ccc"])
+}
+
+locals {
+  files = {
+    "file1.txt" = "Content for file 1",
+    "file2.txt" = "Content for file 2",
+    "file3.txt" = "Content for file 3"
+  }
+}
+
+resource "local_file" "example" {
+  for_each = local.files
+  filename = each.key
+  content  = each.value
 }
