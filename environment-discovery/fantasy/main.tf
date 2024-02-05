5782411da9311f95919fb8ee3e129e4c040a0823
Tomer Landesman <tomer.landesman@env0.com>
2024-02-05 15:03:12 +0200

    sd

--- environment-discovery/fantasy/main.tf
+++
@@ -1,3 +0,0 @@
-resource "null_resource" "example" {
-  # Configuration options for the null resource go here
-}
--- environment-discovery/real-life/main.tf
+++ environment-discovery/real-life/main.tf
@@ -1,3 +1,5 @@
 resource "null_resource" "example" {
   # Configuration options for the null resource go here
+  count = 1
+
 }
