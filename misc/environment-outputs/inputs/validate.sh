d3dc129a4a3c5fdc5a9b901298b8431913a10fb0
Elbaz <eranelbaz97+github@gmail.com>
2024-04-14 17:37:05 +0300

    Update validate.sh

--- misc/environment-outputs/inputs/validate.sh
+++ misc/environment-outputs/inputs/validate.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -e
-if [[ "$INPUT" == "hello" ]];
+if [[ "$SENSITIVE" == "sensitive" ]];
 then
-  echo "Resolved Input correctly"
+  echo "Resolved SENSITIVE input correctly"
   exit 0
 else
-  echo "Didnt resolve input correctly, got - $INPUT"
+  echo "Didnt resolve SENSITIVE input correctly, got - $SENSITIVE"
+  exit 1
+fi
+
+if [[ "$NOT_SENSITIVE" == "not sensitive" ]];
+then
+  echo "Resolved NOT_SENSITIVE input correctly"
+  exit 0
+else
+  echo "Didnt resolve NOT_SENSITIVE input correctly, got - $NOT_SENSITIVE"
   exit 1
 fi
