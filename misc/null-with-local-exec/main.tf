8309f8a8cb4b408ee32f402437e089b8f93ac460
Amit Alkobi <amit.alkobi@env0.com>
2024-04-17 13:07:36 +0300

    demo

--- misc/null-with-local-exec/main.tf
+++ misc/null-with-local-exec/main.tf
@@ -1,4 +1,9 @@
-resource "null_resource" "null" {
+resource "null_resource" "null-resource" {
+
+  triggers = {
+    timestamp = "${timestamp()}"
+  }
+
   provisioner "local-exec" {
     command = "echo hello"
   }
