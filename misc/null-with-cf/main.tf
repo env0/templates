324a535f1b7925ac4e765b4a834b25f1ea357e13
Amit Alkobi <amit.alkobi@env0.com>
2024-04-17 12:58:32 +0300

    demo

--- misc/null-with-cf/main.tf
+++ misc/null-with-cf/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_version = ">= 1.2.0, < 2.0.0"
+
 }
 resource "null_resource" "null" {
+  provisioner "local-exec" {
+    command = 'echo "from env cf: ${ENV0_ENVIRONMENT_ID}"'
+  }
 }
