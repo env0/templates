3e090506de2234f0ce26c6e0a2cedb3a89212f09
Amit Alkobi <amit.alkobi@env0.com>
2024-04-17 13:01:22 +0300

    demo

--- misc/null-with-cf/main.tf
+++ misc/null-with-cf/main.tf
@@ -4,6 +4,6 @@ terraform {
 }
 resource "null_resource" "null" {
   provisioner "local-exec" {
-    command = 'echo "from env cf: ${ENV0_ENVIRONMENT_ID}"'
+    command = "echo \"from env cf: ${ENV0_ENVIRONMENT_ID}\""
   }
 }
