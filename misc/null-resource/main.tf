11d8a60761e3264fe096a3581eae6e055791c07a
Yariv Gavriel <yarivgavriel2@gmail.com>
2024-05-29 12:34:14 +0300

    fix stuff and add 2

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,35 +1,5 @@
 resource "null_resource" "null" {
 }
 
-resource "null_resource2" "null" {
-}
-
-resource "null_resource3" "null" {
-}
-
-resource "null_resource4" "null" {
-}
-
-resource "null_resource5" "null" {
-}
-
-resource "null_resource6" "null" {
-}
-
-resource "null_resource7" "null" {
-}
-
-resource "null_resource8" "null" {
-}
-
-resource "null_resource9" "null" {
-}
-
-resource "null_resource10" "null" {
-}
-
-resource "null_resource11" "null" {
-}
-
-resource "null_resource12" "null" {
+resource "null_resource" "null2" {
 }
