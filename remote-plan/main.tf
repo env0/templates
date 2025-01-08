509d6b694ed2043d690ec182855ac87e5092f29b
Tomer Landesman <tomer.landesman@env0.com>
2025-01-08 13:13:54 +0200

    s

--- remote-plan/main.tf
+++ remote-plan/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  cloud {
-    hostname     = "backend-dev.api.dev.env0.com"
-    organization = "bd12a04a-b051-4f51-9a8a-8a32ac198802.6b1b0897-5322-48cd-98b7-5e11766f54c8"
-    workspaces {
-      name = "product-demo"
-    }
-  }
-
-  required_version = "1.5.7"
-
-}
-
 resource "null_resource" "null" {
   count = 5
 }
@@ -29,3 +16,7 @@ resource "null_resource" "null" {
 #     Test = "yes"
 #   }
 # }
+module "my-module" {
+  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/my-module/tomer"
+  version = "1.1.0"
+}
