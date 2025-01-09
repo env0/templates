43790427bf63dd1ade90ad897272ba0fabd53d3a
Tomer Landesman <tomer.landesman@env0.com>
2025-01-09 12:47:41 +0200

    s

--- module-usage/main.tf
+++ module-usage/main.tf
@@ -1,7 +1,4 @@
-module "env0" {
-  source  = "app.terraform.io/tomer123/env0/provider"
-  version = "1.0.0"
-}
+
 module "Random" {
   source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/Random/env0"
   version = "1.0.0"
