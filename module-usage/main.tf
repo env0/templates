e65e174442e0ae680ab950fd10be0de9bf3827ec
Tomer Landesman <tomer.landesman@env0.com>
2025-01-22 13:35:22 +0200

    s

--- module-usage/main.tf
+++ module-usage/main.tf
@@ -1,7 +1,7 @@
 
-module "tomer" {
-  source = "api-pr17961.dev.env0.com/031eac81-1916-4930-9b7c-8c5dab859d0e/tomer/tomer"
-  version = "1.1.0"
+module "tf-module" {
+  source = "api-pr17961.dev.env0.com/031eac81-1916-4930-9b7c-8c5dab859d0e/tf-module/tomer"
+  version = "0.0.2"
 }
 
 resource "null_resource" "null" {}
