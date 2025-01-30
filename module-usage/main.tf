1e50ab6a3aea743f257e65e6841bbc53ba79672f
Tomer Landesman <tomer.landesman@env0.com>
2025-01-30 18:08:11 +0200

    s

--- module-usage/main.tf
+++ module-usage/main.tf
@@ -1,7 +1,8 @@
 
-
-module "localModule" {
-    source = "./local-module"
+module "moduleName" {
+  source = "api-pr18034.dev.env0.com/fe2a0403-6dc5-4897-913e-9d3efc97473a/moduleName/provider"
+  version = "1.0.0"
 }
+
 
 resource "null_resource" "null" {}
