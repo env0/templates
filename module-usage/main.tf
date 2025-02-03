127901f8a9be3fc9c63ab08de7dd026220c2633c
Tomer Landesman <tomer.landesman@env0.com>
2025-02-03 20:52:18 +0200

    s

--- module-usage/main.tf
+++ module-usage/main.tf
@@ -1,11 +1,11 @@
 
-#   module "localModule" {
-#       source = "./local-module"
-#       version = "1.0.0"
-#   }
+   module "localModule" {
+       source = "./local-module"
+       version = "1.0.0"
+   }
 
-module "vpc" {
-  source = "git::https://github.com/env0/modules-ci-testing-integration-tests.git?ref=pr-test"
-}
+ module "vpc" {
+   source = "git::https://github.com/env0/modules-ci-testing-integration-tests.git?ref=pr-test"
+ }
 
 resource "null_resource" "null" {}
