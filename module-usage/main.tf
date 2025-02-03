d7c6fe5207027804df71a58f5aab5312b84ca44b
Tomer Landesman <tomer.landesman@env0.com>
2025-02-03 21:10:23 +0200

    a

--- module-usage/main.tf
+++ module-usage/main.tf
@@ -2,11 +2,11 @@
 #   module "localModule" {
 #       source = "./local-module"
 #   }
-module "namename" {
-  source = "api-pr18106.dev.env0.com/daa05acb-6df8-40bb-aab2-6949af64026f/namename/providerer"
+
+module "koko" {
+  source = "api-pr18106.dev.env0.com/c53a8298-4679-433c-906e-b4d527710258/koko/lcoc"
   version = "0.0.2"
 }
-
 
  module "vpc" {
    source = "git::https://github.com/env0/modules-ci-testing-integration-tests.git?ref=pr-test"
