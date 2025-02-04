a3227371da38e4aa753f09e223cd8648aa549d37
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-02-04 14:39:37 +0200

    fix custom RBE bucket external id

--- aws/custom-remote-state-bucket/main.tf
+++ aws/custom-remote-state-bucket/main.tf
@@ -6,7 +6,7 @@ module "remote_state_bucket" {
   source = "github.com/env0/remote-state-bucket-module/aws"
   
   # Module-specific inputs
-  external_id        = "ddd43a98-e5a1-46a2-8748-397118c5dfe4"
+  external_id        = "64922d09-2a1f-4f5b-b978-4f5e18f5e3b6"
   state_bucket_name  = "custom-rbe-bucket-integration-tests-states-bucket"
   region             = "us-west-2"
   env0_aws_role_name = "arn:aws:iam::170412844252:role/remote-backend-bors"
