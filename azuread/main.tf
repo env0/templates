e6deb753e2dd7c539f01b46477c34726de1bedae
Tomer Landesman <tomer.landesman@env0.com>
2025-02-16 10:15:35 +0200

    s

--- azuread/main.tf
+++ azuread/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.15.0"
+      version = ">= 3.1.0"  # 👈 This allows upgrades
     }
   }
 }
