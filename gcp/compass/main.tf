00b300b660492c1f8144eb0a09aa382b06c3a557
Tomer Landesman <tomer.landesman@env0.com>
2025-06-04 09:59:23 +0300

    s

--- gcp/compass/main.tf
+++ gcp/compass/main.tf
@@ -5,8 +5,50 @@ provider "google" {
 
 
 #create bucket
-resource "google_storage_bucket" "buc" {
-  name          = "my-name-tomer-bucket-test"
-  location      = "US"
+resource "google_service_account" "default" {
+  account_id   = "my-custom-sa"
+  display_name = "Custom SA for VM Instance"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
 }
 
